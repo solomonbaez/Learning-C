96c2fd19e938805e28a75508b7bb54eb5caa22a1
solomonbaez <solomon.james.baez@gmail.com>
2024-01-11 23:15:17 -0800

    F/C temperature converter

--- ch_1/hello_c.c
+++ ch_1/hello_c.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
+/*test comment ;)*/
+void F_C_Table() {
+  float f, c;
+  float lower, upper, delta;
+
+  lower = 0;
+  upper = 400;
+  delta = 20;
+
+  f = lower;
+  while (f <= upper) {
+    c = (5.0 / 9.0) * (f - 32.0);
+    printf("%3.2f \t %6.2f \n", f, c);
+    f += delta;
+  }
+}
+
+int main() {
   printf("hello, world\n");
+  F_C_Table();
 }
