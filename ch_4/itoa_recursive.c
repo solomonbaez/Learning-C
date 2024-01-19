0f71cbdd4d901d6936a5af9b770d6c7fdbc8b67a
solomonbaez <solomon.james.baez@gmail.com>
2024-01-18 23:34:33 -0800

    Modified reverse to utilize recursion

--- ch_4/itoa_recursive.c
+++ ch_4/itoa_recursive.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 void itoa_recursive(int n, int i, char s[])  {
-  void reverse(int, char []);
+  void reverse_recursive(int, int, char []);
 
   s[i] = n % 10 + '0';
 
   if (n / 10) {
     itoa_recursive(n / 10, ++i, s);
   } else {
-    reverse(i, s);
+    reverse_recursive(0, i, s);
   }
 }
 
-void reverse(int length, char s[]) {
-  int i, j;
-  for (i = 0, j = length; i < j; i++, j--) {
-    char tmp = s[i];
-    s[i] = s[j];
-    s[j] = tmp;
-  }
+void reverse_recursive(int i, int j, char s[]) {
+  char tmp = s[i];
+  s[i] = s[j];
+  s[j] = tmp;
+
+  if (i < j) { reverse_recursive(++i, --j, s); }
 }
 
 #define MAXCHAR 1000
 int main(void) {
   char s[MAXCHAR];
 
-  itoa_recursive(221, 0, s);
+  itoa_recursive(251, 0, s);
   printf("itoa: %s\n", s);
 }
